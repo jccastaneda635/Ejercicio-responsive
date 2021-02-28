236fd229bec018e6973647b4f8ca4e4852c969b9
jccastaneda635 <79811609+jccastaneda635@users.noreply.github.com>
2021-02-28 16:45:21 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+AOS.init();
+
+
+
+function funcionInicial(){
+   var contentUno = document.getElementById('imagenUno');
+   var textoParrafo = document.getElementById('textoModificar');
+   var nombreExperto = document.getElementById('nombreExperto');
+   document.getElementById('imagenUno').src = 'img/personajes1.png';
+   document.getElementById('textoModificar').innerText='Vestibulum eleifend congue odio, eu elementum. Proin magna fringilla felis id, ultrices nisi.Cras ut nulla fermentum, hendrerit lorem ultrices,  sem. Cras sit amet justo ac ipsum luctus congue ut id enim.Pellentesque est magna, rutrum quis aliquet faucibus, tristique nec sapien.';
+   document.getElementById('nombreExperto').innerText='Linda Green';
+   
+}
+function funcionUno(){
+    var contentUno = document.getElementById('imagenUno');
+    var textoParrafo = document.getElementById('textoModificar');
+    var nombreExperto = document.getElementById('nombreExperto');
+    document.getElementById('imagenUno').src = 'img/personajes2.png';
+    document.getElementById('textoModificar').innerText='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, mollitia neque architecto esse porro expedita et nostrum dolorem nisi qui quo vero eos repellat dolor provident accusamus recusandae nemo obcaecati quod quaerat. Eligendi, sunt quas sequi voluptatem neque eum aliquam molestiae beatae. ';
+    document.getElementById('nombreExperto').innerText='Erica Priston';
+ }
+ function funcionDos(){
+    var contentUno = document.getElementById('imagenUno');
+    var textoParrafo = document.getElementById('textoModificar');
+    var nombreExperto = document.getElementById('nombreExperto');
+    document.getElementById('imagenUno').src = 'img/personaje3.png';
+    document.getElementById('textoModificar').innerText='              Vestibulum eleifend congue odio, eu elementum. Proin magna fringilla felis id, ultrices nisi. Cras ut nulla fermentum, hendrerit lorem ultrices,  sem. Cras sit amet justo ac ipsum luctus congue ut id enim. Pellentesque est magna, rutrum quis aliquet faucibus, tristique nec sapien. ';
+    document.getElementById('nombreExperto').innerText='Sam Kromstain';
+ }
+
+//alert("hola mundo");
